61cf289eeaefe05644d9090c20995136864ce60d
ilya <wyster@make.im>
2019-05-03 22:05:20 +0200

    feat: wiktionary search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,17 @@
 <template>
   <div :class="$style.index">
-    <input ref="input" type="text" :class="$style.index__input" autofocus v-model="input" />
+    <input
+      ref="input"
+      type="text"
+      :class="$style.input"
+      autofocus
+      v-model="input"
+    />
+    <div v-if="list.length > 0">
+      <ul :class="$style.list">
+        <li v-for="item in list" :key="item" :class="$style.list__item">{{ item }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -10,25 +21,47 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component
 export default class Index extends Vue {
   private input = '';
+  private list = [];
   mounted() {
     let timeout: number | undefined = undefined;
     this.$watch('input', (v: string) => {
+      this.list = [];
+      if (!v) {
+        return;
+      }
       clearTimeout(timeout);
       timeout = setTimeout(() => this.request(v), 500);
     });
   }
-  request(v: string) {
+  async request(v: string) {
     console.log('send', v);
+    let url = `http://localhost:8081/https://it.wiktionary.org/w/api.php?action=opensearch&format=json&formatversion=2&search=${v}&namespace=0&limit=10`;
+    const response = await fetch(url);
+    this.list = (await response.json() as any)[1];
   }
 }
 </script>
 
 <style lang="less" module>
 .index {
-  &__input {
-    height: 30px;
-    width: 80%;
-    font-size: 30px;
+  width: 80vw;
+  margin: 0 auto;
+}
+
+.input {
+  height: 6vh;
+  font-size: 4vh;
+  width: 100%;
+}
+
+.list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+
+  &__item {
+    text-align: left;
+    line-height: 6vh;
   }
 }
 </style>
